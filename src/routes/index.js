e19bbf663e43cbd5a0624964a1f8d0d75c5ef8c6
Fernanda <fernandacarolineleite360@gmail.com>
2020-07-04 20:01:21 -0300

    Back original routes settings

--- src/routes/index.js
+++ src/routes/index.js
@@ -32,19 +32,15 @@ const Routes = () => {
       <Route path="/signup-company" component={SignUpCompany} />
       <Route path="/type-registration" component={TypeRegistration} />
 
-      <Route
-        path="/home-client"
-        component={HomeClient}
-        /*isPrivate*/
-      />
+      <Route path="/home-client" component={HomeClient} isPrivate />
 
-      <Route path="/home-company" component={HomeCompany} /*isPrivate*/ />
+      <Route path="/home-company" component={HomeCompany} isPrivate />
 
-      <Route path="/products" component={Products} /*isPrivate*/ />
+      <Route path="/products" component={Products} isPrivate />
 
-      <Route path="/add-product" component={AddProduct} /*isPrivate*/ />
+      <Route path="/add-product" component={AddProduct} isPrivate />
 
-      <Route path="/shopping-cart" component={ShoppingCart} /*isPrivate*/ />
+      <Route path="/shopping-cart" component={ShoppingCart} isPrivate />
     </Switch>
   );
 };
