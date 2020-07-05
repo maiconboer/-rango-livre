f023c5a26a28193f53c68bf68c8c04ded339d545
Maicon Boer <maicon_boer@hotmail.com>
2020-07-05 17:40:03 -0300

    adding features

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,13 +4,11 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 
 import SignIn from '../pages/SignIn';
-import TypeRegistration from '../pages/TypeRegistration';
 
 import SignUpClient from '../pages/SignUpClient';
 import SignUpCompany from '../pages/SignUpCompany';
 
 import HomeClient from '../pages/HomeClient';
-import HomeCompany from '../pages/HomeCompany';
 
 import Products from '../pages/Products';
 import AddProduct from '../pages/AddProduct';
@@ -22,21 +20,13 @@ import Deposits from '../pages/Deposits';
 const Routes = () => {
   return (
     <Switch>
-      {/* ADICIONAR isPrivate nas rotas privadas
-      exemplo:
-      <Route path="/home-client" component={HomeClient} isPrivate /> */}
-
-      {/* <Route path="/" exact component={SignIn} /> */}
 
       <Route path="/" exact component={SignIn} />
 
       <Route path="/signup-client" component={SignUpClient} />
       <Route path="/signup-company" component={SignUpCompany} />
-      <Route path="/type-registration" component={TypeRegistration} />
 
       <Route path="/home-client" component={HomeClient} isPrivate />
-
-      <Route path="/home-company" component={HomeCompany} isPrivate />
 
       <Route path="/products" component={Products} isPrivate />
 
